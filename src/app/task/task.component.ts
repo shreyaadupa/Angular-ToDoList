ea38ae7f6163f09776178fb795f3a45bf452887e
shreyaadupa <81499294+shreyaadupa@users.noreply.github.com>
2022-09-07 14:37:19 -0400

    2 files modified

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,32 +1,84 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+ selector: 'app-task',
+ templateUrl: './task.component.html',
+ styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  public tasks = [];
-  public newTask;
-
-   addTask() {
-
-      if(this.newTask != '')
-      {
-        this.tasks.push(this.newTask);
-        this.newTask = '';
-      }
+ 
+ constructor() { }
+ 
+ ngOnInit() {
+ }
+ 
+ public doneTasks = [];
+ public tasks = [];
+ public newTask;
+ public markdoneTask;
+ public Priority;
+ public Status;
+ public index;
+ 
+  addTask() //will not be needed soon
+  {
+     if(this.newTask != '')
+     {
+       this.tasks.push(this.newTask);
+       this.newTask = '';
+     }
+   }
+ 
+   addHighTask()//insert at top of list
+   {
+     if(this.newTask != '')
+     {
+       this.tasks.unshift(this.newTask);
+       this.newTask = '';
+       this.Priority = '';
+     }
+   }
+ 
+   addMediumTask() //insert after the last high task OR at top if no high task
+   {
+ 
+   }
+ 
+   addLowTask()//insert at bottom of list
+   {
+     if(this.newTask != '')
+     {
+       this.tasks.push(this.newTask);
+       this.newTask = '';
+       this.Priority = '';
+     }
+   }
+ 
+   addCompletedTask(index) //make sure to strike through the task as well in html
+   {
+     if(this.newTask != '')
+     {
+       this.tasks.push(this.newTask);
+       this.newTask = '';
+       this.Priority = '';
+     }
+   }
+ 
+   doneTask(index)//if task marked as done add to done table below
+   {
+     this.tasks.splice(index, 1);
+     this.doneTasks.push(this.markdoneTask);
+   }
+ 
+    deleteTask(index: number) //delete one specific task
+    {
+       this.tasks.splice(index, 1);
     }
-  
-
-     deleteTask(index) {
-        this.tasks.splice(index, 1);
-    }
+ 
+ 
+   clearTasks() //clear all tasks from table
+   {
+     this.doneTasks.length = 0;
+   }
+ 
 }
-
